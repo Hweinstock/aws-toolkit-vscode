9e2135088209c59f07d490a52bd41daf9fb7832c
hkobew <hkobew@amazon.com>
2024-11-20 10:01:41 -0500

    unnest tests

--- packages/core/src/test/shared/defaultAwsClientBuilderV3.test.ts
+++ packages/core/src/test/shared/defaultAwsClientBuilderV3.test.ts
@@ -19,6 +19,8 @@ import { Client } from '@aws-sdk/smithy-client'
 import { extensionVersion } from '../../shared'
 import { assertTelemetry } from '../testUtil'
 import { telemetry } from '../../shared/telemetry'
+// import { EC2Client } from '@aws-sdk/client-ec2'
+// import { mockClient } from 'aws-sdk-client-mock'
 
 describe('DefaultAwsClientBuilderV3', function () {
     let builder: AWSClientBuilderV3
@@ -27,40 +29,43 @@ describe('DefaultAwsClientBuilderV3', function () {
         builder = new DefaultAWSClientBuilderV3(new FakeAwsContext())
     })
 
-    describe('createAndConfigureSdkClient', function () {
-        it('includes Toolkit user-agent if no options are specified', async function () {
-            const service = await builder.createAwsService(Client as any)
-            const clientId = getClientId(new GlobalState(new FakeMemento()))
+    it('includes Toolkit user-agent if no options are specified', async function () {
+        const service = await builder.createAwsService(Client as any)
+        const clientId = getClientId(new GlobalState(new FakeMemento()))
 
-            assert.ok(service.config.customUserAgent)
-            assert.strictEqual(
-                service.config.customUserAgent![0][0].replace('---Insiders', ''),
-                `AWS-Toolkit-For-VSCode/testPluginVersion Visual-Studio-Code/${version} ClientId/${clientId}`
-            )
-            assert.strictEqual(service.config.customUserAgent![0][1], extensionVersion)
+        assert.ok(service.config.customUserAgent)
+        assert.strictEqual(
+            service.config.customUserAgent![0][0].replace('---Insiders', ''),
+            `AWS-Toolkit-For-VSCode/testPluginVersion Visual-Studio-Code/${version} ClientId/${clientId}`
+        )
+        assert.strictEqual(service.config.customUserAgent![0][1], extensionVersion)
+    })
+
+    it('adds region to client', async function () {
+        const service = await builder.createAwsService(Client as any, { region: 'us-west-2' })
+
+        assert.ok(service.config.region)
+        assert.strictEqual(service.config.region, 'us-west-2')
+    })
+
+    it('adds Client-Id to user agent', async function () {
+        const service = await builder.createAwsService(Client as any)
+        const clientId = getClientId(new GlobalState(new FakeMemento()))
+        const regex = new RegExp(`ClientId/${clientId}`)
+        assert.ok(service.config.customUserAgent![0][0].match(regex))
+    })
+
+    it('does not override custom user-agent if specified in options', async function () {
+        const service = await builder.createAwsService(Client as any, {
+            customUserAgent: [['CUSTOM USER AGENT']],
         })
 
-        it('adds region to client', async function () {
-            const service = await builder.createAwsService(Client as any, { region: 'us-west-2' })
+        assert.strictEqual(service.config.customUserAgent[0][0], 'CUSTOM USER AGENT')
+    })
 
-            assert.ok(service.config.region)
-            assert.strictEqual(service.config.region, 'us-west-2')
-        })
-
-        it('adds Client-Id to user agent', async function () {
-            const service = await builder.createAwsService(Client as any)
-            const clientId = getClientId(new GlobalState(new FakeMemento()))
-            const regex = new RegExp(`ClientId/${clientId}`)
-            assert.ok(service.config.customUserAgent![0][0].match(regex))
-        })
-
-        it('does not override custom user-agent if specified in options', async function () {
-            const service = await builder.createAwsService(Client as any, {
-                customUserAgent: [['CUSTOM USER AGENT']],
-            })
-
-            assert.strictEqual(service.config.customUserAgent[0][0], 'CUSTOM USER AGENT')
-        })
+    it('logs each API request', async function () {
+        //const ec2MockClient = mockClient(EC2Client)
+        //const service = await builder.createAwsService(ec2MockClient)
     })
 })
 
